78cb5574df6ebc267b3515215b3fd2ef61b045cb
Samriddhi Sivakumar <73621618+SamriddhiS2@users.noreply.github.com>
2023-12-11 21:24:59 -0800

    Updated frontend

--- public/search/search.js
+++ public/search/search.js
@@ -9,7 +9,7 @@
 
 "use strict";
 
-(function() {
+(function () {
   window.addEventListener("load", init);
 
   /**
@@ -21,7 +21,7 @@
   function init() {
     let searchBar = document.getElementById("search");
 
-    searchBar.addEventListener("click", showFilteredItems);
+    searchBar.addEventListener("input", showFilteredItems);
   }
 
   /**
@@ -29,8 +29,71 @@
    * inputs text to search for in the
    * search bar
    */
-  function showFilteredItems() {
-    // Show filtered items after user searches
+  async function showFilteredItems() {
+    try {
+      const searchInput = document.getElementById("search").value;
 
+      // Fetch filtered items based on user's input
+      const response = await fetch(`/api/classes/search?searchQuery=${searchInput}`);
+      const classes = await response.json();
+
+      if (response.ok) {
+        displayFilteredItems(classes);
+      } else {
+        console.error("Failed to fetch filtered items:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching filtered items:", error);
+    }
+  }
+
+  /**
+   * Displays the filtered items on the page.
+   * @param {Array} classes - An array of filtered class objects.
+   */
+  function displayFilteredItems(classes) {
+    const sections = document.querySelectorAll("section ul");
+
+    // Clear existing content
+    sections.forEach((section) => (section.innerHTML = ""));
+
+    // Iterate over classes and create list items
+    classes.forEach((classInfo) => {
+      const listItem = createListItem(classInfo);
+      const section = document.querySelector(`section h2:contains('${classInfo.major}')`).nextElementSibling;
+
+      section.appendChild(listItem);
+    });
+  }
+
+  /**
+   * Creates a list item for a filtered class.
+   * @param {Object} classInfo - Information about a class.
+   * @returns {HTMLLIElement} - The created list item element.
+   */
+  function createListItem(classInfo) {
+    const listItem = document.createElement("li");
+    const addButton = document.createElement("button");
+    addButton.textContent = "+";
+
+    // Add click event listener to the button
+    addButton.addEventListener("click", () => handleAddButtonClick(classInfo.class_id));
+
+    const className = document.createElement("a");
+    className.textContent = classInfo.class_name;
+
+    listItem.appendChild(className);
+    listItem.appendChild(addButton);
+
+    return listItem;
+  }
+
+  /**
+   * Handles the logic when the add button is clicked for a class.
+   * @param {string} classId - The ID of the selected class.
+   */
+  function handleAddButtonClick(classId) {
+    // Implement the logic to add the class to the user's selection or perform other actions
+    console.log("Add button clicked for class: " + classId);
   }
 })();

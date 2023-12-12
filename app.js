c47095dcc5ea174952747387b9b375afd6cfa838
evaliu2002 <evaxinerliu@gmail.com>
2023-12-12 01:14:33 -0800

    jslinter

--- app.js
+++ app.js
@@ -477,8 +477,7 @@ app.post('/api/classes/classesTaken', async (req, res) => {
     } else {
       let db = await getDBConnection();
 
-      let classDetails = await db.get(
-        'SELECT class_id FROM Classes WHERE class_name = ?', className);
+      let classDetails = await db.get('SELECT class_id FROM Classes WHERE class_name = ?', className);
 
       if (!classDetails) {
         handleDoesNotExist(res, 'Class does not exist.');
